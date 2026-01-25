e7b966b0f09c0a2d9d58df4bc75f2448d9531578
Sakshi13-05 <sakshichavan61116@gmail.com>
2026-01-25 18:55:29 +0530

    Setting up the basic communication between frontend backend

--- Frontend/my-app/src/main.jsx
+++ Frontend/my-app/src/main.jsx
@@ -1,8 +1,9 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
+import { StrictMode } from 'react' // Change this
+import { createRoot } from 'react-dom/client' // Modern way to import createRoot
 import App from './App.jsx'
+import './index.css'
 
+// Use the imported named functions directly
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <App />
---
+++ Frontend/my-app/src/pages/SubmitGrievance.jsx
@@ -0,0 +1,52 @@
+import React,{useState} from "react";
+
+function SubmitGrievance (){
+   const [des,setDes]=useState("");
+   const [file,setFile]=useState(null);
+   const [status,setStatus]=useState("");
+
+   const handleUpload= async (e)=>{
+        e.preventDefault();
+
+        //create object  to send
+        const formData=new FormData();
+        formData.append("description",des);
+        formData.append("file",file);
+
+        try{
+            setStatus("Uploading...");
+            const res = await fetch("http://127.0.0.1:8000/submit-complaint", {
+                method: "POST",
+                body: formData
+            });
+
+            const response=await res.json();
+            if(response.ok){
+                setStatus("Success! complaint ID: "+res.message);
+                setDes("");
+                setFile(null);
+            }
+
+
+        }catch(err){
+            console.log(err);
+            setStatus("Backend is offline");
+        }
+   }
+
+   return(
+    <>
+    <h2>Submit</h2>
+    <form action="" onSubmit={handleUpload}>
+        <label htmlFor="">Describe the issue:</label><br/>
+        <textarea name="" id=""/><br/><br/>
+
+        <label htmlFor="">Upload Photo</label><br/>
+        <input type="file" /><br/><br/>
+
+        <button type="submit">Submit</button>
+    </form>
+    <p>{status}</p>
+    </>);
+}
+export default SubmitGrievance;

75824a2daeabc9aa88ea3c00a0bd714acceb5f2b
Sakshi13-05 <sakshichavan61116@gmail.com>
2026-01-25 11:08:40 +0530

    Establishing Routes

--- Frontend/my-app/src/App.jsx
+++ Frontend/my-app/src/App.jsx
@@ -1,10 +1,19 @@
 import './App.css'
+import React from 'react'
+import {BrowserRouter as Router,Routes,Route} from 'react-router-dom';
+import SubmitGrievance from './pages/SubmitGrievance'
 
 function App() {
-  
-
   return (
     <>
+    <Router>
+      <div className='App'>
+        {/* Navigation here */}
+      </div>
+      <Routes>
+        <Route path="submit" element={SubmitGrievance}/>
+      </Routes>
+    </Router>
     </>
   )
 }
